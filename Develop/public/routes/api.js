7802286048b7036a70d0321f19b3e4eec652793f
ChemaKing13 <jm2000ho@gmail.com>
2023-05-29 13:39:12 -0600

    file paths updated

--- Develop/public/routes/api.js
+++ Develop/public/routes/api.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const fs = require('fs');
+const express = require('express'); 
+const fs = require('fs'); 
 const path = require('path');
-const router = express.Router();
+const router = express.Router(); //this create an instance of the express router, allow us to define routes and their handlers
 
 // Get all notes
 router.get('/notes', (req, res) => {
@@ -21,18 +21,22 @@ router.get('/notes', (req, res) => {
 });
 
 // Create a new note
+//first sets the up the POST route
 router.post('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, './db/db.json'), 'utf-8', (err, data) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), 'utf-8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Failed to read notes from the database' });
       return;
     }
 
+    //htsi parses the content of the db.json into a js object
     const notes = JSON.parse(data);
 
-    const newNoteID = Math.random().toString(36).substr(2, 9);
+    const currentDate = new Date(); 
+    const newNoteID = currentDate.getTime().toString();  //this generates a unique by using the current timestamp as the ID
 
+    //this creates a new note objetc
     const newNote = {
       id: newNoteID,
       title: req.body.title,
@@ -43,7 +47,7 @@ router.post('/notes', (req, res) => {
     notes.push(newNote);
 
     // Write the updated notes array back to the db.json file
-    fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(notes), (err) => {
+    fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
         res.status(500).json({ error: 'Failed to save the new note' });
@@ -55,5 +59,7 @@ router.post('/notes', (req, res) => {
   });
 });
 
+
+//exports the router module so that it can be used in other files 
 module.exports = router;
 
