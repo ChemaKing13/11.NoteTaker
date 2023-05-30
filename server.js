cc4c36f4aced7aa27214f10ef957d765096c837c
ChemaKing13 <jm2000ho@gmail.com>
2023-05-30 17:26:32 -0600

    html set into public

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const apiRoutes = require('./routes/api'); 
 
-
-
 const PORT = process.env.PORT || 3004; 
 
 const app = express(); 
@@ -13,20 +11,24 @@ const app = express();
 app.use(clog); 
 
 //Middleware for parsing JSON and urlencoded from data
-app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);  
+app.use(express.json()); 
 app.use(express.static('public'));
+
 
 //Get route for homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 }); 
+
+
+app.use('/api', apiRoutes);  
+
 
 //GET route for the notes page
 //its defining a route for the URL path /notes, using the HTTP GET method and sends the notes.html file
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './notes.html')); 
+  res.sendFile(path.join(__dirname, './public/notes.html')); 
 });
 
 
